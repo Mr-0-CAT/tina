7d41295ed463f1f823c90250d2c6aaf091d36cc6
Mr-0-CAT <155630072+Mr-0-CAT@users.noreply.github.com>
2024-05-23 00:15:18 +0530

    .

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,135 @@
 module.exports = function(eleventyConfig) {
     // Return your Object options:
     eleventyConfig.addPassthroughCopy('./src/style.css')
-    eleventyConfig.addPassthroughCopy('./src/assets')
+    eleventyConfig.addPassthroughCopy('./src/responsive.css')
+    eleventyConfig.addPassthroughCopy('./src/events')
+    eleventyConfig.addPassthroughCopy('./src/About/style.css')
+    eleventyConfig.addPassthroughCopy('./src/About/responsive.css')
+    eleventyConfig.addPassthroughCopy('./src/Contact/responsive.css')
+    eleventyConfig.addPassthroughCopy('./src/Contact/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Contact/reixicon.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/responsive.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/style.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/actkrit/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/actkrit/responsive.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/actkrit/swiper-bundle.min.css')
+   
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/AdroIT/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/AdroIT/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/AMURA/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/AMURA/responsive.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/AMURA/swiper-bundle.min.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/AURA/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/AURA/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Aikya/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Aikya/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/BigO/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/BigO/responsive.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/BigO/swiper-bundle.min.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Cat/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Cat/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/CodEX/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/CodEX/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Cybertrox/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Cybertrox/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/DCG/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/DCG/responsive.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/DCG/swiper-bundle.min.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Dhwani/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Dhwani/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/E yantra/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/E yantra/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/EDC/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/EDC/responsive.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/EDC/swiper-bundle.min.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Enlightera/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/enlightera/responsive.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/enlightera/swiper-bundle.min.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Esponica/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Esponica/responsive.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Esponica/swiper-bundle.min.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Falcon/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Falcon/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/GDSC/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/GDSC/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/IEEE/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/IEEE/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/IETE/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/IETE/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/MUN/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/MUN/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Nataranj/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Nataranj/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Nex sus/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Nex sus/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/nss/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/nss/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/PDC/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/PDC/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/sci-fi/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/sci-fi/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/SHUDHI/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/SHUDHI/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/RSC/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/RSC/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Splash/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Splash/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/YRSC/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/YRSC/responsive.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Pinnacle/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/pinnacle/responsive.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/pinnacle/swiper-bundle.min.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/ROBOTRONICS/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/ROBOTRONICS/responsive.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/ROBOTRONICS/swiper-bundle.min.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Synapse/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Synapse/responsive.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/Synapse/swiper-bundle.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/UDC/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/UDC/responsive.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/UDC/swiper-bundle.min.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/V-tech/style.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/V-tech/responsive.css')
+    eleventyConfig.addPassthroughCopy('./src/Our Clubs/clubs/V-tech/swiper-bundle.min.css')
+   
+    eleventyConfig.addPassthroughCopy('./src/admin/config.yml')
+   
+   
     eleventyConfig.addPassthroughCopy('./src/logo.webp')
-
-    eleventyConfig.addPassthroughCopy('./tina')
-
 
     return {
       dir: {
